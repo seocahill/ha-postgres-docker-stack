adbe24a90d082c37ae66586b7a9da6d9d0933162
Seosamh Cahill <seo.cahill@gmail.com>
2017-10-09 16:48:42 +0200

    Tidy up prod stack
    More renaming of files

--- config/etcd.sh
+++ config/etcd.sh
@@ -1,12 +1,12 @@
 !# /bin/sh
 
-etcd -name etcdnode1 \
+etcd -name etcd \
     --data-dir /etcd_data \
-    -advertise-client-urls http://etcdnode1:2379 \
+    -advertise-client-urls http://etcd:2379 \
     -listen-client-urls http://0.0.0.0:2379 \
-    -initial-advertise-peer-urls http://etcdnode1:2380 \
+    -initial-advertise-peer-urls http://etcd:2380 \
     -listen-peer-urls http://0.0.0.0:2380 \
-    -initial-cluster etcdnode1=http://etcdnode1:2380
+    -initial-cluster etcd=http://etcd:2380
 
 # sleep 10
 
